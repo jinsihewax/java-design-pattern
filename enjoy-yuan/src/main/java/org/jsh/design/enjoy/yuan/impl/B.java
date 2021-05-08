1aa8ca52609c64360d9c99cf793c3997debc15bf
jinsihe <jinsihe@tass.com.cn>
2021-05-08 21:19:32 +0800

    享元模式

--- enjoy-yuan/src/main/java/org/jsh/design/enjoy/yuan/impl/B.java
+++ enjoy-yuan/src/main/java/org/jsh/design/enjoy/yuan/impl/B.java
@@ -2,13 +2,28 @@ package org.jsh.design.enjoy.yuan.impl;
 
 import org.jsh.design.enjoy.yuan.A;
 import org.springframework.stereotype.Component;
+// 享元模式
+//@Component
+//public class B implements A {
+//
+//	@Override
+//	public void doTest() {
+//		System.out.println("B");
+//	}
+//
+//}
 
+//享元模式+深克隆 
 @Component
-public class B implements A {
+public class B implements A, Cloneable {
 
 	@Override
 	public void doTest() {
 		System.out.println("B");
 	}
 
+	@Override
+	public B clone() throws CloneNotSupportedException {
+		return (B) super.clone();
+	}
 }
--- enjoy-yuan/src/test/java/org/jsh/design/enjoy/yuan/EnjoyYuanTest.java
+++ enjoy-yuan/src/test/java/org/jsh/design/enjoy/yuan/EnjoyYuanTest.java
@@ -22,4 +22,17 @@ public class EnjoyYuanTest {
 		b.doTest();
 	}
 
+	@Test
+	public void testCloneObject() throws CloneNotSupportedException {
+		// 使用一个对象
+		B b = a.getTyg("b");
+		B b1 = b.clone();
+		System.out.println("b:" + System.identityHashCode(b));
+		System.out.println("old-b:" + System.identityHashCode(b));
+		System.out.println("b1:" + System.identityHashCode(b1));
+		
+		//普通new 性能比clone快
+		//稍微复杂构造方法，new性能比clone差太多。
+	}
+
 }
